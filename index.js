054a39fb13478a913fdebac2ed1b39d02d939ead
asfiqurrahmanomey <omeyjkt@gmail.com>
2022-12-07 01:04:23 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,378 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const nodemailer = require("nodemailer");
+const mg = require('nodemailer-mailgun-transport');
+const stripe = require("stripe")(process.env.STRIPE_SECRET)
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// * Middle Ware * //
+app.use(cors());
+app.use(express.json());
+
+// * MongoDB Connection * //
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.9vhsktv.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// * Functions for SandGrid * //
+function sendBookingEmail(booking) {
+    const { email, treatment, appointmentDate, slot } = booking;
+
+    // This is your API key that you retrieve from www.mailgun.com/cp (free up to 10K monthly emails)
+    const auth = {
+        auth: {
+            api_key: process.env.EMAIL_SEND_KEY,
+            domain: process.env.EMAIL_SEND_DOMAIN
+        }
+    }
+
+    const transporter = nodemailer.createTransport(mg(auth));
+
+    // * SandMailer Transporter * //
+    // let transporter = nodemailer.createTransport({
+    //     host: 'smtp.sendgrid.net',
+    //     port: 587,
+    //     auth: {
+    //         user: "apikey",
+    //         pass: process.env.SENDGRID_API_KEY
+    //     }
+    // });
+
+    // * SandGrid  transporter setup * //
+    transporter.sendMail({
+        from: "omeyjkt@gmail.com", // verified sender email
+        to: email, // recipient email
+        subject: `Your Appointment for ${treatment} Confirmed`, // Subject line
+        text: "Hello world!",
+        // plain text body
+        html: `
+        <h3> Your Appointment is Confirmed</h3>
+        <div>
+            <p>Your Appointment for Treatment: ${treatment}</p>
+            <p>Please Visit us on: ${appointmentDate} at ${slot}</p>
+            <p>Thanks from Doctor Service</p>
+        </div>
+        `, // html body
+    }, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+
+}
+
+// * Verify JWT * //
+function verifyJWT(req, res, next) {
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('Unauthorized accessToken');
+    }
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Access Forbidden' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+// * Find Multiple Documents Using CURD * //
+async function run() {
+    try {
+        // * Appointment Time * //
+        const appointmentOptionCollection = client.db('doctors-portal').collection('appointment-Time');
+        // * Booking Collection * //
+        const bookingsCollection = client.db('doctors-portal').collection('bookings');
+        // * User Collection * //
+        const usersCollection = client.db('doctors-portal').collection('users');
+        // * Doctors Collection * //
+        const doctorsCollection = client.db('doctors-portal').collection('doctors');
+        // * Payment Collection * //
+        const paymentsCollection = client.db('doctors-portal').collection('payments');
+
+
+        // * Verify Admin * //
+        const verifyAdmin = async (req, res, next) => {
+            const decodedEmail = req.decoded.email;
+            const query = { email: decodedEmail };
+            const user = await usersCollection.findOne(query);
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ message: 'Access Forbidden' })
+            }
+            next();
+        }
+
+        // * Get Appointment Date  from Database * //
+        /* System to marge data */
+        app.get('/appointment-Time', async (req, res) => {
+            const date = req.query.date;
+            const query = {};
+            const options = await appointmentOptionCollection.find(query).toArray();
+            // * Get the booking date to provide date * //
+            const bookingQuery = { appointmentDate: date }
+            const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray();
+
+            options.forEach(option => {
+                const optionBooked = alreadyBooked.filter(book => book.treatment === option.name);
+                const bookedSlots = optionBooked.map(book => book.slot);
+                // * Remaining Slots * //
+                const remainingSlots = option.slots.filter(slot => !bookedSlots.includes(slot))
+                options.slots = remainingSlots;
+            })
+            res.send(options);
+        });
+
+        // * API V2 * //
+        app.get('/v2/appointment-Time', async (req, res) => {
+            const date = req.query.date;
+            const options = await appointmentOptionCollection.aggregate([
+                {
+                    $lookup: {
+                        from: 'bookings',
+                        localField: 'name',
+                        foreignField: 'treatment',
+                        pipeline: [
+                            {
+                                $match: {
+                                    $expr: {
+                                        $eq: ['$appointmentDate', date]
+                                    }
+                                }
+                            }
+                        ],
+                        as: 'booked'
+                    }
+                },
+                {
+                    $project: {
+                        name: 1,
+                        price: 1,
+                        slots: 1,
+                        booked: {
+                            $map: {
+                                input: '$booked',
+                                as: 'book',
+                                in: '$$book.slot'
+                            }
+                        }
+                    }
+                },
+                {
+                    $project: {
+                        name: 1,
+                        price: 1,
+                        slots: {
+                            $setDifference: ['$slots', '$booked']
+                        }
+                    }
+                }
+            ]).toArray();
+            res.send(options);
+        })
+
+        // * * //
+        app.get('/appointmentSpecialty', async (req, res) => {
+            const query = {}
+            const result = await appointmentOptionCollection.find(query).project({ name: 1 }).toArray();
+            res.send(result);
+        })
+
+        // * Get Booking Data from Database * //
+        app.get('/bookings', verifyJWT, async (req, res) => {
+            const email = req.query.email;
+            const decodedEmail = req.decoded.email;
+
+            if (email !== decodedEmail) {
+                return res.status(403).send({ message: 'Forbidden Access' })
+            }
+
+            const query = { email: email };
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        })
+
+        // * Get Specific Booking with ID * //
+        app.get('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const booking = await bookingsCollection.findOne(query);
+            res.send(booking);
+            console.log(booking);
+        })
+
+
+        // * Booking Data Post * //
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            console.log(booking);
+            // * Search appointment on present date * //
+            const query = {
+                appointmentDate: booking.appointmentDate,
+                email: booking.email,
+                treatment: booking.treatment
+            }
+            // * Appointment on Present Date * //
+            const existingDateCount = await bookingsCollection.find(query).toArray();
+            if (existingDateCount.length) {
+                const message = `You Have a Booking Today on ${booking.appointmentDate}`
+                return res.send({ acknowledged: false, message });
+            }
+            /*...................................*/
+            const result = await bookingsCollection.insertOne(booking);
+            // * SendMailer Email * //
+            sendBookingEmail(booking);
+            res.send(result);
+        });
+
+        // * Stripe Generate * //
+        app.post('/create-payment-intent', async (req, res) => {
+            const booking = req.body;
+            const price = booking.price;
+            const amount = price * 100;
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: 'usd',
+                amount: amount,
+                "payment_method_types": [
+                    "card"
+                ]
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        })
+
+        // * Store Payment * //
+        app.post('/payments', async (req, res) => {
+            const payment = req.body;
+            const result = await paymentsCollection.insertOne(payment);
+            const id = payment.bookingId
+            const filter = { _id: ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    paid: true,
+                    transactionId: payment.transactionId
+                }
+            }
+            const updatedResult = await paymentsCollection.updateOne(filter, updatedDoc);
+            res.send(result);
+        })
+
+        //  * Generate Token * //
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            // * Check User * //
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+                return res.send({ accessToken: token });
+            }
+            res.status(403).send({ accessToken: '' })
+        })
+
+        // *  Get All Users * //
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const users = await usersCollection.find(query).toArray();
+            res.send(users);
+        })
+
+        // * Get Specific Admin * //
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email };
+            const user = await usersCollection.findOne(query);
+            res.send({ isAdmin: user?.role === 'admin' });
+        })
+
+
+        // * User Data Post * //
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+
+        // * Make Admin * //
+        app.put('/users/admin/:id', verifyJWT, verifyAdmin, async (req, res) => {
+
+            // const decodedEmail = req.decoded.email;
+            // const query = { email: decodedEmail };
+            // const user = await usersCollection.findOne(query);
+            // if (user?.role !== 'admin') {
+            //     return res.status(403).send({ message: 'Access Forbidden' })
+            // }
+
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        });
+
+        // // * temporary Update price field insert * //
+        // app.get('/addPrice', async (req, res) => {
+        //     const filter = {}
+        //     const options = { upsert: true }
+        //     const updatedDoc = {
+        //         $set: {
+        //             price: 99
+        //         }
+        //     }
+        //     const result = await appointmentOptionCollection.updateMany(filter,  updatedDoc, options);
+        //     res.send(result);
+        // })
+
+
+        // * GET Doctors Collection * //
+        app.get('/doctors', verifyJWT, verifyAdmin, async (req, res) => {
+            const query = {};
+            const doctors = await doctorsCollection.find(query).toArray();
+            res.send(doctors);
+        })
+
+        // * Post Doctors Collection * //
+        app.post('/doctors', verifyJWT, verifyAdmin, async (req, res) => {
+            const doctor = req.body;
+            const result = await doctorsCollection.insertOne(doctor);
+            res.send(result);
+        })
+
+        // * Delete Doctor Data * //
+        app.delete('/doctors/:id', verifyJWT, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const result = await doctorsCollection.deleteOne(filter);
+            res.send(result);
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.log)
+
+
+app.get('/', async (req, res) => {
+    res.send('Server Running')
+});
+
+
+app.listen(port, () => console.log(`Doctors Running on Port ${port}`))
